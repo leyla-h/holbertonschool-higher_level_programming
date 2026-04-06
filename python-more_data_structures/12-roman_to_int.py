aac8837dc34075ea716894dcf4b9424e43b96efb
leyla-h <13678@holbertonstudents.com>
2026-04-06 22:46:02 +0400

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,9 @@ def roman_to_int(roman_string):
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and rom_n[roman_string[i]] < rom_n[roman_string[i + 1]]:
-            res -= rom_n[roman_string[i]]
+        val = rom_n.get(roman_string[i], 0)
+        if i + 1 < len(roman_string) and val < rom_n.get(roman_string[i + 1], 0):
+            res -= val
         else:
-            res += rom_n[roman_string[i]]
+            res += val
     return res
