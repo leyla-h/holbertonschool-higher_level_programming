e6f12f6c128db9f47d01a3d5f0aaefd7d974cad5
leyla-h <13678@holbertonstudents.com>
2026-04-06 22:20:02 +0400

    Create 10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    max_key = None
+    max_val = 0
+
+    for key, value in a_dictionary.items():
+        if max_key is None or value > max_val:
+            max_val = value
+            max_key = key
+            
+    return max_key
